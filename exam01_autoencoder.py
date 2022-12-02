98d5695b06e28ddc5f685ba3bd17d1078cd16cf3
scolp <scolpig@gmail.com>
2022-12-02 16:38:11 +0900

    first commit

--- exam01_autoencoder.py
+++ exam01_autoencoder.py
@@ -1,1 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.datasets import mnist
+
+input_img = Input(shape=(784,))
+encoded = Dense(32, activation='relu')
+encoded = encoded(input_img)
+decoded = Dense(784, activation='sigmoid')
+decoded = decoded(encoded)
+autoencoder = Model(input_img, decoded)
+autoencoder.summary()
+
+
+
+
+
+
